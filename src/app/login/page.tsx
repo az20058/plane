b964bc6e05ec428752b786a142db26e4831117b2
YeChan Kim <az20058@naver.com>
2024-11-18 20:04:23 +0900

    Fix : 로그인 직후에 네비게이션 바 정보가 변경되지 않는 버그 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,63 +1,73 @@
 "use client";
-import styles from '@/styles/login/login.module.css';
-import axios from 'axios';
-import { useRef } from 'react';
-import cookie from 'js-cookie';
-import { useRouter } from 'next/navigation';
+import styles from "@/styles/login/login.module.css";
+import axios from "axios";
+import { useRef } from "react";
+import cookie from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+  const idRef = useRef<HTMLInputElement>(null);
+  const pwRef = useRef<HTMLInputElement>(null);
 
-    const router = useRouter();
-    const idRef = useRef<HTMLInputElement>(null);
-    const pwRef = useRef<HTMLInputElement>(null);
-
-    function handleKeyDown(event: React.KeyboardEvent) {
-      if (event.key === 'Enter') {
-          handleSubmit();
-      }
+  function handleKeyDown(event: React.KeyboardEvent) {
+    if (event.key === "Enter") {
+      handleSubmit();
     }
-    
-    function handleSubmit() {
-        axios.post((`${process.env.NEXT_PUBLIC_SERVER_URL}/login`), null, {
-            params: {
-                username: idRef.current?.value,
-                password: pwRef.current?.value
-            }
-        })
-        .then(res=>{
-            axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/userDto`, {
-              headers: {
-                Authorization: res.headers['authorization']
-              }
-            })
-              .then(res=>{
-                cookie.set("username", res.data.split(' ')[0]);
-              })
-              .catch(err=>{
-                console.error("userDto error", err);
-              })
-            router.push(`/`);
-            router.refresh();
-          })
-          .catch(()=>{
-            alert("아이디 또는 비밀번호가 일치하지 않습니다.");
-          })
-    }
+  }
 
-    return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.contentWrapper}>
-                <div className={styles.header}>
-                    <span>로그인을 진행해 주세요</span>
-                </div>
-                <div className={styles.inputWrapper}>
-                    <input ref={idRef} placeholder='아이디를 입력하세요' onKeyDown={handleKeyDown}/>
-                    <input type='password' ref={pwRef} placeholder='비밀번호를 입력하세요' onKeyDown={handleKeyDown}/>
-                </div>
-                <div className={styles.btnWrapper}>
-                    <button onClick={handleSubmit}>계속 진행하기</button>
-                </div>
-            </div>
+  function handleSubmit() {
+    axios
+      .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/login`, null, {
+        params: {
+          username: idRef.current?.value,
+          password: pwRef.current?.value,
+        },
+      })
+      .then((res) => {
+        axios
+          .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/userDto`, {
+            headers: {
+              Authorization: res.headers["authorization"],
+            },
+          })
+          .then((res) => {
+            cookie.set("username", res.data.split(" ")[0]);
+          })
+          .catch((err) => {
+            console.error("userDto error", err);
+          });
+        router.push(`/`);
+        router.refresh();
+      })
+      .catch(() => {
+        alert("아이디 또는 비밀번호가 일치하지 않습니다.");
+      });
+  }
+
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.header}>
+          <span>로그인을 진행해 주세요</span>
         </div>
-    )
+        <div className={styles.inputWrapper}>
+          <input
+            ref={idRef}
+            placeholder="아이디를 입력하세요"
+            onKeyDown={handleKeyDown}
+          />
+          <input
+            type="password"
+            ref={pwRef}
+            placeholder="비밀번호를 입력하세요"
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className={styles.btnWrapper}>
+          <button onClick={handleSubmit}>계속 진행하기</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/UserInfo.tsx
+++ src/components/navbar/UserInfo.tsx
@@ -1,29 +1,30 @@
 "use client";
-import styles from '@/styles/navbar.module.css';
-import { useEffect, useState } from 'react';
-import { usegetLogin } from '@/hooks/getLogin';
-import { usePathname } from 'next/navigation';
+import styles from "@/styles/navbar.module.css";
+import { useEffect, useState } from "react";
+import { usegetLogin } from "@/hooks/getLogin";
+import { usePathname } from "next/navigation";
+import Cookies from "js-cookie";
 
 export default function UserInfo() {
-    const [username, setUsername] = useState<string|undefined>('');
-    const pathname = usePathname();
-    const [mounted, setMounted] = useState(false);
+  const [username, setUsername] = useState<string | undefined>("");
+  const pathname = usePathname();
+  const [mounted, setMounted] = useState(false);
+  const token = Cookies.get("token");
 
-    useEffect(()=>{
-        setUsername(usegetLogin());
-    }, [pathname])
+  useEffect(() => {
+    setUsername(usegetLogin());
+  }, [pathname, token]);
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-    
-    return (
-        mounted&&username?
-        <div className={styles.user}>
-            <span className={styles.username}>{username} 님 </span>
-            <span>로그인</span>
-        </div>
-        :
-        <></>
-    )
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted && username ? (
+    <div className={styles.user}>
+      <span className={styles.username}>{username} 님 </span>
+      <span>로그인</span>
+    </div>
+  ) : (
+    <></>
+  );
 }
