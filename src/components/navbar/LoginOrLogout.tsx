e582f5d76e85ff32a27c3a6f324b6406e245d78e
YeChan Kim <az20058@naver.com>
2024-11-18 20:44:11 +0900

    Fix : Navbar를 클라이언트 컴포넌트로 변경

--- src/components/navbar/LoginOrLogout.tsx
+++ src/components/navbar/LoginOrLogout.tsx
@@ -1,34 +1,28 @@
 "use client";
 import Link from "next/link";
 import { usegetLogin } from "@/hooks/getLogin";
-import cookie from 'js-cookie';
+import cookie from "js-cookie";
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
-export default function LoginOrLogout() {
-    const [username, setUsername] = useState<string|undefined>('');
-    const pathname = usePathname();
-    const [mounted, setMounted] = useState(false);
-    const router = useRouter();
+interface Props {
+  username: string | undefined;
+}
 
-    useEffect(()=>{
-        setUsername(usegetLogin());
-        console.log("change path");
-    }, [pathname]);
-    
-    useEffect(() => {
-        setMounted(true);
-      }, []);
+export default function LoginOrLogout({ username }: Props) {
+  const pathname = usePathname();
+  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
 
-    function handleLogout() {
-        cookie.remove("username");
-        if(pathname==="/")
-            window.location.reload();
-        else
-            router.push("/");
-    }
-    
-    return (
-        mounted&&username?<button onClick={handleLogout}>로그아웃</button>:<Link href='/login'>로그인</Link>
-    )
+  function handleLogout() {
+    cookie.remove("username");
+    if (pathname === "/") window.location.reload();
+    else router.push("/");
+  }
+
+  return username ? (
+    <button onClick={handleLogout}>로그아웃</button>
+  ) : (
+    <Link href="/login">로그인</Link>
+  );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,8 +5,22 @@ import navLogo from "../../../public/images/navLogo.svg";
 import Link from "next/link";
 import UserInfo from "./UserInfo";
 import LoginOrLogout from "./LoginOrLogout";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { usegetLogin } from "@/hooks/getLogin";
 
 export default function Navbar() {
+  const [username, setUsername] = useState<string | undefined>("");
+  const pathname = usePathname();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setUsername(usegetLogin());
+    console.log("change path");
+  }, [pathname]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.menuWrapper}>
@@ -16,7 +30,7 @@ export default function Navbar() {
           </Link>
         </div>
         <div className={styles.menuItem}>
-          <LoginOrLogout />
+          <LoginOrLogout username={username} />
         </div>
         <div className={styles.menuItem}>
           <Link href="/signUp">회원가입</Link>
