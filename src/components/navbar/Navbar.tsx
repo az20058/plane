309a12353cc34787d48aa92cc884f7d1ea7f271c
YeChan Kim <az20058@naver.com>
2024-11-18 20:35:05 +0900

    Fix : Navbar 컴포넌트를 클라이언트 컴포넌트로 변경

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,31 @@
-import styles from '../../styles/navbar.module.css';
-import Image from 'next/image';
-import navLogo from '../../../public/images/navLogo.svg';
-import Link from 'next/link';
-import UserInfo from './UserInfo';
-import LoginOrLogout from './LoginOrLogout';
+"use client";
+import styles from "../../styles/navbar.module.css";
+import Image from "next/image";
+import navLogo from "../../../public/images/navLogo.svg";
+import Link from "next/link";
+import UserInfo from "./UserInfo";
+import LoginOrLogout from "./LoginOrLogout";
 
 export default function Navbar() {
-    return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.menuWrapper}>
-                <div className={styles.logo}>
-                    <Link href="/"><Image src={navLogo} alt='Logo in Navbar'/></Link>
-                </div>
-                <div className={styles.menuItem}>
-                    <LoginOrLogout/>
-                </div>
-                <div className={styles.menuItem}>
-                    <Link href='/signUp'>회원가입</Link>
-                </div>
-                <div className={styles.menuItem}>
-                    <Link href='/checkRes'>구매 내역</Link>
-                </div>
-            </div>
-            <UserInfo/>
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.menuWrapper}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src={navLogo} alt="Logo in Navbar" />
+          </Link>
         </div>
-    )
+        <div className={styles.menuItem}>
+          <LoginOrLogout />
+        </div>
+        <div className={styles.menuItem}>
+          <Link href="/signUp">회원가입</Link>
+        </div>
+        <div className={styles.menuItem}>
+          <Link href="/checkRes">구매 내역</Link>
+        </div>
+      </div>
+      <UserInfo />
+    </div>
+  );
 }
